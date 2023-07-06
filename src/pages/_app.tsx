9a1bbe0dcf1c8c05fd1f484773b44ffdd6d05a10
carloshkruger <carloshkruger@gmail.com>
2023-07-06 20:48:07 -0300

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import { AppProps } from "next/app"
+import Image from "next/image"
+import { globalStyles } from "../styles/global"
+import { Container, Header } from "../styles/pages/app"
+
+import logoImg from '../assets/logo.svg'
+
+globalStyles()
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
+}
+
+export default App
